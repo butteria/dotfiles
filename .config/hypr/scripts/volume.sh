533a99414d7614c63610f1177f06c42e632c6cd0
butteria <1340075229@qq.com>
2023-08-10 11:07:48 +0800

    add eww.

--- .config/hypr/scripts/volume.sh
+++ .config/hypr/scripts/volume.sh
@@ -21,11 +21,11 @@ function send_notification {
 #bar=$(seq -s "─" $(($volume/5)) | sed 's/[0-9]//g')
 if [ "$volume" = "0" ]; then
         icon_name="/usr/share/icons/Faba/48x48/notifications/notification-audio-volume-muted.svg"
-$DIR/notify-send.sh "$volume""      " -i "$icon_name" -t 2000 -h int:value:"$volume" -h string:synchronous:"─" --replace=555
+$DIR/notify-send.sh "$volume""  " -i "$icon_name" -t 2000 -h int:value:"$volume" -h string:synchronous:"─" --replace=555
     else
 	if [  "$volume" -lt "10" ]; then
 	     icon_name="/usr/share/icons/Faba/48x48/notifications/notification-audio-volume-low.svg"
-$DIR/notify-send.sh "$volume""     " -i "$icon_name" --replace=555 -t 2000
+$DIR/notify-send.sh "$volume""  " -i "$icon_name" --replace=555 -t 2000
     else
         if [ "$volume" -lt "30" ]; then
             icon_name="/usr/share/icons/Faba/48x48/notifications/notification-audio-volume-low.svg"
@@ -40,7 +40,7 @@ $DIR/notify-send.sh "$volume""     " -i "$icon_name" --replace=555 -t 2000
 fi
 bar=$(seq -s "─" $(($volume/5)) | sed 's/[0-9]//g')
 # Send the notification
-$DIR/notify-send.sh "$volume""     ""$bar" -i "$icon_name" -t 2000 -h int:value:"$volume" -h string:synchronous:"$bar" --replace=555
+$DIR/notify-send.sh "$volume""  ""$bar" -i "$icon_name" -t 2000 -h int:value:"$volume" -h string:synchronous:"$bar" --replace=555
 
 }
 
